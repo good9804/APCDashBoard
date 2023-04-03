a04531fd54b3688ad8ba6917452f381afa542370
Korjw <fkrk56@naver.com>
2023-04-03 15:22:09 +0900

    2023_04_03

--- back/app.js
+++ back/app.js
@@ -30,7 +30,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/dashboard", imagesRouter);
-app.use("/kafka", kafkaRouter);
+app.use("/dashboard/kafka", kafkaRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
