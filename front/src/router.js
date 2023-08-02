10b5ad3afe3507c18ac1f50b32e91d7cffa77183
good9804 <ggoggoni98@gmail.com>
2023-08-02 17:24:48 +0900

    url 변경

--- front/src/router.js
+++ front/src/router.js
@@ -47,7 +47,7 @@ router.beforeEach(async function (to, _, next) {
     console.log(access_message);
     if (access_message == 0) {
       alert("로그인 후 이용해주세요.");
-      window.location.href = 'http://localhost/users/login';
+      window.location.href = 'https://19c4-118-36-223-138.ngrok-free.app/users/login';
     } else {
       next();
     }
@@ -60,10 +60,10 @@ router.beforeEach(async function (to, _, next) {
       next();
     } else if(access_message==0){
       alert("로그인 후 이용해주세요.");
-      window.location.href = 'http://localhost/users/login';
+      window.location.href = 'https://19c4-118-36-223-138.ngrok-free.app/users/login';
     }else {
       alert("관리자 계정만 접근 가능합니다.");
-      window.location.href = 'http://localhost/users/login';
+      window.location.href = 'https://19c4-118-36-223-138.ngrok-free.app/users/login';
     }
   }
   if (to.meta.requiresRole) {
