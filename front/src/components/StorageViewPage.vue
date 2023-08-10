34836814e05b368dff77b3c3eb522aa9969361e8
good9804 <ggoggoni98@gmail.com>
2023-08-10 13:55:12 +0900

    webgl 링크변경

--- front/src/components/StorageViewPage.vue
+++ front/src/components/StorageViewPage.vue
@@ -10,7 +10,7 @@
               id="iframeExample"
               width="1100"
               height="605"
-              src="https://b2b8-118-36-223-138.ngrok-free.app/webgl1/"
+              src="https://b2b8-118-36-223-138.ngrok-free.app/webgl2/"
               frameborder="0"
               framespacing="0"
               marginheight="0"
